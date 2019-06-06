4cad00b1e41047b94db60ae6d71a8f085ec40491
Cristina Luz <crisgolfinho@gmail.com>
2019-06-06 20:39:49 -0300

    ejs

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ const routes = require('./routes/index');
 const  users = require('./routes/users');
 const app = express();
 
-
+app.use('/', routes);
+app.use('/users', users);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.post('/login', function (req, res) {
+  res.render('home/login.ejs'); // load the index.ejs file
+});
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
@@ -17,15 +21,15 @@ app.get('/', function(req, res) {
 
 
 
-app.get("/login",  (req, res) => {
+// app.get("/login",  (req, res) => {
 
-    return res.send('./app/login.ejs');
-  });
+//     return res.send('./app/login.ejs');
+//   });
   
- app.post("/login",  (req, res) => {
+//  app.post("/login",  (req, res) => {
   
-    return res.render('login');
-  });
+//     return res.render('login');
+//   });
 
 
 app.listen(process.env.PORT || 4000, function(){
