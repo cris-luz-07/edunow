e601a9151b0b469c3d91dea53e77ad0bce1d3187
Cristina Luz <crisgolfinho@gmail.com>
2019-06-06 20:32:24 -0300

    ejs

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const routes = require('./routes/index');
+const  users = require('./routes/users');
+const app = express();
+
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.set('view','ejs');
+
 
 app.get("/login",  (req, res) => {
 
---
+++ routes/user.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+
+/* GET home page. */
+router.get('/user', function(req, res) {
+    res.send('user');
+});
+
+module.exports = router;
