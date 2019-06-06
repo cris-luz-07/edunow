1736d3b1bff1e7d66ededc24bec1b8584c1cb3db
Cristina Luz <crisgolfinho@gmail.com>
2019-06-06 20:21:01 -0300

    ejs

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.set('view','ejs');
+
+app.get("/login",  (req, res) => {
+
+    return res.send('./app/login.ejs');
+  });
+  
+ app.post("/login",  (req, res) => {
+  
+    return res.render('login');
+  });
+
+
+app.listen(process.env.PORT || 4000, function(){
+    console.log('Your node js server is running');
+});
+
+
+// const express = require ('express');
+// const app = express;
+
